d924fc0092b8f48a99da7068eb9bfa8a487b7c24
Luis <luiscoimbraeng@outlook.com>
2022-04-16 13:56:41 -0300

    Asset Queries to Apollo

--- src/api/AssetQueries.js
+++ src/api/AssetQueries.js
@@ -1,3 +1,24 @@
+import { gql } from "@apollo/client";
+
+export const ASSETS = gql`
+  query Assets {
+    assets {
+      _id
+      name
+      image
+      description
+      model
+      owner
+      status
+      health_level
+      unit {
+        _id
+        name
+      }
+    }
+  }
+`;
+
 export const queryGetAssets = `{
   assets {
     _id
@@ -15,9 +36,26 @@ export const queryGetAssets = `{
   }
 }`;
 
+
+export const DELETE_ASSET = gql`
+  mutation DeleteAsset($_id: ID!) {
+    deleteAsset(_id: $_id) {
+      _id
+    }
+  }
+`;
+
 export const queryDeleteAsset = (_id) => `mutation {
   deleteAsset(_id: "${_id}")
 }`;
+
+export const ADD_ASSET = gql`
+  mutation CreateAsset($name: String!, $image: String!, $description: String!, $model: String!, $owner: String!, $status,: String! $health_level: String!, $unit: String!) {
+      createAsset(AssetInput:{name: $name, image: $image, description: $description, model: $model, owner: $owner, status: $status, health_level: $health_level, unit: $unit}) {
+        _id
+      }
+  }
+`;
 
 export const queryAddAsset = (inputData) => `mutation {
         createAsset(AssetInput:{name: "${inputData.name}", image: "${inputData.image}", description: "${inputData.description}", model: "${inputData.model}", owner: "${inputData.owner}", status: ${inputData.status}, health_level: ${inputData.health_level}, unit: "${inputData.unit}"}) {
@@ -25,11 +63,49 @@ export const queryAddAsset = (inputData) => `mutation {
           }
       }`;
 
+export const EDIT_ASSET = gql`
+  mutation EditAsset($_id: ID!, $name: String!, $image: String!, $description: String!, $model: String!, $owner: String!, $status,: String! $health_level: String!, $unit: String!) {
+    editAsset(_id: $_id, AssetInput:{name: $name, image: $image, description: $description, model: $model, owner: $owner, status: $status, health_level: $health_level, unit: $unit}) {
+        _id
+        name
+        image
+        description
+        model
+        owner
+        status
+        health_level
+        unit {
+          _id
+          name
+        }
+      }
+  }
+`;
+
 export const queryEditAsset = (inputData) => `mutation {
   editAsset(_id: "${inputData._id}", AssetInput: {name: "${inputData.name}", image: "${inputData.image}", description: "${inputData.description}", model: "${inputData.model}", owner: "${inputData.owner}", status: ${inputData.status}, health_level: ${inputData.health_level}, unit: "${inputData.unit}"}) {
       _id
   }
 }`;
+
+export const ASSETS_LOGS = gql`
+  query AssetsLogs {
+    assetsLogs {
+      _id
+      type
+      datetime
+      asset{
+      name
+      }
+      responsible{
+        name
+      }
+      value
+      updatedAt
+      createdAt
+    }
+  }
+`;
 
 export const queryGetAssetsLogs = `{
   assetsLogs {
@@ -48,6 +124,27 @@ export const queryGetAssetsLogs = `{
   }
 }`;
 
+
+export const ASSETS_LOG = gql`
+  query AssetsLog($_id: ID!) {
+    assetsLog(_id: $_id) {
+      _id
+      type
+      datetime
+      asset{
+      name
+      }
+      responsible{
+        name
+      }
+      value
+      updatedAt
+      createdAt
+      }
+  }
+`;
+
+
 export const queryGetAssetsLog = (asset_id) => `{
   assetsLog(_id: "${asset_id}") {
     _id
@@ -65,11 +162,38 @@ export const queryGetAssetsLog = (asset_id) => `{
   }
 }`;
 
+export const ADD_ASSET_LOG = gql`
+  mutation CreateAssetLog($type: String!, $datetime: String!, $asset: ID!, $responsible: String!, $value: Float!) {
+    createAssetLog(AssetLogInput:{type: $type, datetime: $datetime, asset: $asset, responsible: $responsible, value: $value}) {
+        _id
+        type
+        datetime
+        asset{
+        name
+        }
+        responsible{
+          name
+        }
+        value
+        updatedAt
+        createdAt
+      }
+  }
+`;
+
 export const queryAddAssetLog = (inputData) => `mutation {
     createAssetLog(AssetLogInput: {type: ${inputData.type}, datetime: "${inputData.datetime}", asset: "${inputData.asset}", responsible: "${inputData.responsible}", value: ${inputData.value}}){
       _id
     }
 }`;
+
+export const DELETE_ASSET_LOG = gql`
+  mutation DeleteAssetLog($_id: ID!) {
+    deleteAssetLog(_id: $_id) {
+      _id
+    }
+  }
+`;
 
 export const queryDeleteAssetLog = (_id) => `mutation {
   deleteAssetLog(_id: "${_id}"){
